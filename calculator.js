f3a9c5c110507bc3e03be1c6c1277d880d1f12c8
Álvaro Rodríguez <alvarorgtr@gmail.com>
2014-08-05 18:38:54 +0200

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,62 @@
+var operatorsByPrecedence = ["+", "-", "/", "*"];
+var operatorHash = {
+	"*": function(a,b){return a * b;},
+	"/": function(a,b){return a / b;},
+	"+": function(a,b){return a + b;},
+	"-": function(a,b){return a - b;}
+};
+
+function calculate(expression) {
+	if (!expression) {
+		return 0;
+	}
+	return evaluate(removeSpacesOf(expression));
+}
+
+function evaluate(expression) {
+	if (!hasOperator(expression)) {
+		return parseInt(expression);
+	}
+
+	var i = 0;
+	var result = 0;
+	for (i = 0; i < operatorsByPrecedence.length; i++) {
+		var operator = operatorsByPrecedence[i];
+		result = operate(expression, operator, operatorHash[operator]);
+		console.log(result);
+		if (result != null) {
+			break;
+		}
+	}
+	return result;
+}
+
+function removeSpacesOf(string) {
+	return string.replace(/\s+/g, '');
+}
+
+function hasCharacter(string, character) {
+	return string.indexOf(character) !== -1;
+}
+
+function splitStringOnFirstOccurrence(string, character){
+	var index = string.indexOf(character);
+	if (index !== -1) {
+		return [string.substring(0, index), string.substring(index + 1)];
+	}
+
+	return [string];
+}
+
+function hasOperator(string) {
+	return hasCharacter(string, "+") || hasCharacter(string, "-") || hasCharacter(string, "*") || hasCharacter(string, "/")
+}
+
+function operate(expression, operator, operation) {
+	if (hasCharacter(expression, operator)) {
+		var splitted = splitStringOnFirstOccurrence(expression, operator);
+		return operation(evaluate(splitted[0]), evaluate(splitted[1]));
+	}
+
+	return null;
+}
